f56ef45ea5ee27f909a258282ab808a814ae45b3
Geselle <CHRISTOPHER.GESELLE@HAUNI.COM>
2017-06-21 14:27:58 +0200

    9

--- messages/index.js
+++ messages/index.js
@@ -9,9 +9,9 @@ var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
 var path = require('path');
 const cognitiveServices = require('cognitive-services'); 
-const textAnalytics = new cognitiveServices.textAnalytics({
-     API_KEY: '74f79220e9af438ca623d96758a4c36c'
- });
+// const textAnalytics = new cognitiveServices.textAnalytics({
+     // API_KEY: '74f79220e9af438ca623d96758a4c36c'
+ // });
 
 var Forecast = require("forecast");
    var forecast = new Forecast({
