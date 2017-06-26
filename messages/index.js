2e570cf7005b1630478f8e7873966dc5b38c1d7a
Chris686 <CHRISTOPHER.GESELLE@HAUNI.COM>
2017-06-26 13:05:23 +0200

    11

--- messages/index.js
+++ messages/index.js
@@ -120,7 +120,7 @@ bot.dialog('profile', [
         builder.Prompts.text(session, 'Hi! What is your name?');
     },
     function (session, results) {
-        session.send("Are you " + results.response)
+       // session.send("Are you " + results.response)
 		session.userData.name = results.response;
         session.endDialog();
     }
