9f54043082499023b108a3b6799576039c6799e3
Chris686 <CHRISTOPHER.GESELLE@HAUNI.COM>
2017-06-27 12:47:57 +0200

    12

--- messages/index.js
+++ messages/index.js
@@ -47,7 +47,11 @@ const LuisModelUrl = 'https://' + luisAPIHostName + '/luis/v1/application?id=' +
 
 // Main dialog with LUIS
 var recognizer = new builder.LuisRecognizer(LuisModelUrl);
-//var intents = new builder.IntentDialog({ recognizers: [recognizer] })
+var intents = new builder.IntentDialog({ recognizers: [recognizer] })
+.macthes('Greeting','dialogGreeting')
+.onDefault((session) => {
+    session.send('Sorry, I did not understand \'%s\'.', session.message.text);
+});
 /*
 .matches('<yourIntent>')... See details at http://docs.botframework.com/builder/node/guides/understanding-natural-language/
 */
@@ -91,19 +95,20 @@ var recognizer = new builder.LuisRecognizer(LuisModelUrl);
 bot.dialog('/', intents);    */
 
 
-bot.recognizer(recognizer);
+//bot.recognizer(recognizer);
 bot.dialog('dialogGreeting', [
 		function (session) {
 			session.send('You asked for weather');
 			//builder.Prompts.text(session, 'Hi! What is your name?');
 		}
-		// ,
-		// function (session, results) {
+		,
+		 function (session, results) {
+			 session.send('You asked for weather');
 			// if(results)
 				// {
 					// builder.Prompts.text(session, results);
 				// }
-		// }
+		 }
 	]).triggerAction({
 	matches: 'Greeting'
 });
