7fd36ca3be9b928d87b5f63f032b9296764ac1c8
Chris686 <CHRISTOPHER.GESELLE@HAUNI.COM>
2017-06-28 14:25:15 +0200

    13

--- messages/index.js
+++ messages/index.js
@@ -131,7 +131,7 @@ function thankYou(sessions, args, next){
 
 function getUserName(session, args, next){
 	session.dialogData.entities = args.entities;
-	var username = builder.EntityRecognuzer.findEntity(args.entities, 'username');
+	var username = builder.EntityRecognizer.findEntity(args.entities, 'username');
 	if(username){
 		session.send("hi " + ", How are you!");
 		next();
