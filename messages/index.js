718b03dc6446ac443fc9d0c95d4907c624b3dd93
Chris686 <CHRISTOPHER.GESELLE@HAUNI.COM>
2017-06-30 12:24:11 +0200

    15

--- messages/index.js
+++ messages/index.js
@@ -138,7 +138,7 @@ function checkSentiment(session, args, next) {
 function getWeather(session, args, next) {
     session.send(args.response.entity + '___' + JSON.stringify(args.response.entity));
     session.send(JSON.stringify(session.userData.locations));
-    session.send(JSON.parse(session.userData.locations[args.response.entity].toString()).Latitude);
+    session.send(JSON.stringify(session.userData.locations[args.response.entity]));
     //var coords = JSON.parse(session.userData.locations[args.response.entity]);
 
     // Initialize
