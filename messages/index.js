cbd9a6ec575325f7c98b744eef7694146ceb1545
Chris686 <CHRISTOPHER.GESELLE@HAUNI.COM>
2017-06-30 12:09:48 +0200

    15

--- messages/index.js
+++ messages/index.js
@@ -138,7 +138,8 @@ function checkSentiment(session, args, next) {
 function getWeather(session, args, next) {
     session.send(args.response.entity + '___' + JSON.stringify(args.response.entity));
     session.send(JSON.stringify(session.userData.locations));
-    var coords = JSON.parse(session.userData.locations);
+    //var coords = JSON.parse(session.userData.locations[args.response.entity]);
+
     // Initialize
     var forecast = new Forecast({
         service: 'darksky',
